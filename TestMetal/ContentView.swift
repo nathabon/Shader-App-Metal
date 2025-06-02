3f7a84e78f487c65a092a74157fc6ad518eb9ebb
nathabon <nath.bontoux@gmail.com>
2025-06-03 00:07:20 +0200

    Initial Commit

---
+++ TestMetal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestMetal
+//
+//  Created by Nathanaël BONTOUX on 03/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestMetal/TestMetalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMetalApp.swift
+//  TestMetal
+//
+//  Created by Nathanaël BONTOUX on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestMetalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
